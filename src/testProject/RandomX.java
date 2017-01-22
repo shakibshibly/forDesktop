d97de5d20d064319cbcdaf79f0b69620435ec3fa
shakibshibly <shakib.shibly@gmail.com>
2017-01-21 22:46:39 -0500

    This is the first javacode commit.
    Change-Id: I6102444c71bc1d7c35fd0cdc0ff52d91a96f7a0c
    Signed-off-by: shakibshibly <shakib.shibly@gmail.com>

---
+++ src/testProject/RandomX.java
@@ -0,0 +1,48 @@
+package testProject;
+
+import java.util.Random;
+
+public class RandomX {
+
+	public static void main(String[] args) {
+
+		// (integer) numOfFloatingNum variable to represent how many floating
+
+		// point random number user want to generate
+
+		int numOfFloatingNum = 260;
+
+		Random r = new Random();
+
+		System.out.println("------------------------------------------------------------------------------------");
+
+		System.out.println(numOfFloatingNum + " uniformly distributed randomly generated floaing-point numbers: ");
+
+		System.out.println("------------------------------------------------------------------------------------");
+
+		System.out.println();
+
+		// generate a uniformly distributed float random numbers
+
+		float[] floats = new float[numOfFloatingNum];
+
+		for (int i = 0; i < floats.length; i++) {
+
+			floats[i] = r.nextFloat() * 20 - 10;
+
+		}
+
+		for (float f : floats) {
+
+			for (int k = 1; k <= 15; k++) {
+
+				System.out.printf("%-2.6f%-4s", f, " ");
+
+			}
+
+			System.out.println();
+
+		}
+	}
+}
+
